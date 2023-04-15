c99580e8c47b11360bc11c2dc095cade2726858b
hyeonbeen97 <125417850+hyeonbeen97@users.noreply.github.com>
2023-04-15 16:30:52 +0900

    1
    1

---
+++ ex/06/js/solution-1.js
@@ -0,0 +1,34 @@
+
+let quonte = document.querySelector(".wise__quote");
+let author = document.querySelector(".wise__author");
+let wiseAll = []; 
+let item = [];
+const dataQuestion = () => {
+  fetch("../json/dummy01.json")  
+    .then(res => res.json())       
+    .then(items => {                
+
+      let totIndex = Math.round(Math.random() * items.quotes.length);
+      quonte.innerHTML = ` ${items.quotes[totIndex].id}. ${items.quotes[totIndex].quote} `;
+      author.innerHTML = `- ${items.quotes[totIndex].author}`;
+      items.quotes.forEach(element => {
+
+
+        item.push({
+          id: element.id,
+          quote: element.quote,
+          author: element.author
+        });
+      });
+    })
+    .catch((err) => console.log(err));
+}
+
+dataQuestion();
+
+setInterval(() => {
+
+  let totIndex = Math.round(Math.random() * item.length);
+  quonte.innerHTML = `${item[totIndex].id}. ${item[totIndex].quote} `;
+  author.innerHTML = `- ${item[totIndex].author}`;
+}, 1000);   
