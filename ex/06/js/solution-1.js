061b3f192ceca944c04f2684df0211485479fb2e
hyeonbeen97 <125417850+hyeonbeen97@users.noreply.github.com>
2023-04-18 13:16:10 +0900

    123

--- ex/06/js/solution-1.js
+++ ex/06/js/solution-1.js
@@ -4,12 +4,26 @@ let author = document.querySelector(".wise__author");
 let wiseAll = []; 
 let item = [];
 
+
+const preloadImages = () => {
+  const images = [
+    "https://source.unsplash.com/random/?0",
+    "https://source.unsplash.com/random/?1",
+    "https://source.unsplash.com/random/?2",
+  ];
+
+  images.forEach((src) => {
+    const img = new Image();
+    img.src = src;
+  });
+};
+
 const getRandomBackground = () => {
   const randomIndex = Math.floor(Math.random() * 10);
-  return `url('https://source.unsplash.com/random/?${randomIndex}')`;
+  return `url('https://source.unsplash.com/random/?${randomIndex}')`
 }
 
-      
+
 
 const dataQuestion = () => {
   fetch("../json/dummy01.json")  
@@ -39,13 +53,14 @@ dataQuestion();
 
 setInterval(() => {
   dataQuestion();
+  
 
   let totIndex = Math.round(Math.random() * item.length);
   quonte.innerHTML = `${item[totIndex].id}. ${item[totIndex].quote} `;
   author.innerHTML = `- ${item[totIndex].author}`;
   document.querySelector("body").style.transition = "background-image 0.1s";
   document.querySelector("body").style.backgroundImage = getRandomBackground();
-
+  
   }, 1000);
   
    
