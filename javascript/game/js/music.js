6a85b2933748be6b9bfb2cac7fce22bb80091185
hyeonbeen97 <125417850+hyeonbeen97@users.noreply.github.com>
2023-04-26 00:18:15 +0900

    1

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -1,53 +1,53 @@
 const allMusic = [
     {
-        name : "1.노래제목",
-        artist : "Park",
+        name : "1.Dangerously",
+        artist : "Charlie Puth",
         img : "music_view01",
         audio : "music_audio01"
 
     }, {
-        name : "2.노래제목",
-        artist : "Park",
+        name : "2.낭만교향곡",
+        artist : "빅나티",
         img : "music_view01",
         audio : "music_audio02"
     }, {
-        name : "3.노래제목",
-        artist : "Park",
+        name : "3.첫사랑",
+        artist : "백아",
         img : "music_view01",
         audio : "music_audio03"
     }, {
-        name : "4.노래제목",
-        artist : "Park",
+        name : "4.INDUSTRY BABY",
+        artist : "Lil Nas X Jack Harlow",
         img : "music_view01",
         audio : "music_audio04"
     }, {
-        name : "5.노래제목",
-        artist : "Park",
+        name : "5.빛이 나는 너에게 ",
+        artist : "던(DAWN)",
         img : "music_view01",
         audio : "music_audio05"
     }, {
-        name : "6.노래제목",
-        artist : "Park",
+        name : "6.인사",
+        artist : "범진",
         img : "music_view01",
         audio : "music_audio06"
     }, {
-        name : "7.노래제목",
-        artist : "Park",
+        name : "7.Rainy day",
+        artist : "PATEKO 파테코(Feat.ASH ISLAND Skinny Brown) ",
         img : "music_view01",
         audio : "music_audio07"
     }, {
-        name : "8.노래제목",
-        artist : "Park",
+        name : "8.연애의 발명",
+        artist : "빅나티",
         img : "music_view01",
         audio : "music_audio08"
     }, {
-        name : "9.노래제목",
-        artist : "Park",
+        name : "9.Tired",
+        artist : "Alan Walker ft Gavin James",
         img : "music_view01",
         audio : "music_audio09"
     }, {
-        name : "10.노래제목",
-        artist : "Park",
+        name : "10.INVU",
+        artist : "태연",
         img : "music_view01",
         audio : "music_audio10"
     }
