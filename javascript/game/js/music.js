3bc5f84f99a78e0712658651c8dd4c65bb89aaad
hyeonbeen97 <125417850+hyeonbeen97@users.noreply.github.com>
2023-04-25 18:18:09 +0900

    12

---
+++ javascript/game/js/music.js
@@ -0,0 +1,55 @@
+const allMusic = [
+    {
+        name : "1.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio01"
+
+    }, {
+        name : "2.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio02"
+    }, {
+        name : "3.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio03"
+    }, {
+        name : "4.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio04"
+    }, {
+        name : "5.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio05"
+    }, {
+        name : "6.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio06"
+    }, {
+        name : "7.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio07"
+    }, {
+        name : "8.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio08"
+    }, {
+        name : "9.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio09"
+    }, {
+        name : "10.노래제목",
+        artist : "Park",
+        img : "music_view01",
+        audio : "music_audio10"
+    }
+
+]
